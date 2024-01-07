ae7bf507c97ab2ea525c39d8619d82c8a76fd98a
gabuser <trabalhoinvest100@gmail.com>
2024-01-07 12:30:26 -0300

    delete function add

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-#o script precisa fornecer qual caminho deve se mover os arquivos
-# o script precisa conter a função de mover mais de um arquivo ou e permitir que o script possa 
-#mover todos da estrutura de uma vez
-# o script precisa verificar se  os arquivos mencionados estão dentro do repositório e retornar 
-#uma msg de erro caso não
-#repetir o preocesso
-
-
-#será perguntado ao usuário qual diretório os arquivos estão 
-#em seguida iremo usar o os module para mover o usuário para o local onde estão os arquivos
-#a classe irá herdar esse atributos e instanciar ao objeto
-#
-#a próxima função chamada copiar irá herdar esse atributos e usar o while para permitir que 
-#cada loop o usuário possa mover os arquivos para um destino desejado
-#
-#vamos usar um for loop para fazer uma busca sequencial e verificar se os itens estão dentro 
-#de uma variável que iremos criar para armazenar todos os arquivos listados
-#
-#vamos usar o if statment, o if stament permite que o computador possa tomar uma decisão de acordo 
-#com os dados de entrada.
-#
-#e retornar uma msg de erro caso não esteja dentro e se caso estiver perdir para o usuário 
-#colocar o novo destino
-
-
 import os, shutil 
 
 class usuario:
@@ -47,13 +22,47 @@ class usuario:
             
             if arquivo == 'q':
                 self.rodar=False
+
+class deletar(usuario):
+
+    def __init__(self,localiz):
+        usuario.__init__(self, localiz)
     
+    def apagar(self):
+        self.remover=True
+        self.arquivos=os.listdir()
+        print(f'seus arquivos: {self.arquivos}')
+
+        while self.remover:
+            escolh=input('insira os arquivos:')
+            for c in self.arquivos:
+                if escolh == c: 
+                    os.remove(f'{self.localiz}/{escolh}')
+            if escolh == 'q':
+                self.remover=False
+            
+
+esclh=input('o que deseja fazer a(apagar arquivos) c(copiar arquivos):')
 caminho=input('insira o caminho de onde estão os arquivos:')
 
-try:
-    mover=usuario(caminho)
+if esclh== 'c':
+    try:
+        mover=usuario(caminho)
+        mover.copiar()
+    except FileNotFoundError:
+        print('diretório inexistente')
 
-    mover.copiar()
-except FileNotFoundError:
-    
-    print('verifique se voce colocou o diretório corretamente: /home/user/...')
+else:
+    try:
+        tirar=deletar(caminho)
+        tirar.apagar()
+    except FileNotFoundError:
+        print('diretório inexistente')
+
+#try:
+#    mover=usuario(caminho)
+#
+#    mover.copiar()
+#except FileNotFoundError:
+#    
+#    print('verifique se voce colocou o diretório corretamente: /home/user/...')
